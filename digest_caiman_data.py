8c69415149d011cc5f58dca39fb77f8548df4249
Vladislav Ivantaev <108753041+ivantaev@users.noreply.github.com>
2025-08-13 14:59:54 +0200

    Update digest_caiman_data.py

--- digest_caiman_data.py
+++ digest_caiman_data.py
@@ -10,7 +10,7 @@ Loads the data produced by CaImAn software, computes:
     SI (or SHC) value, statistics
     firing rate maps, occupancy
 
-@author: Vlad
+@author: Vladislav Ivantaev
 """
 # delete all the variables
 for name in list(globals()):
@@ -21,7 +21,7 @@ del name
 import glob
 import os
 from datetime import date
-from PF_analysis_CL_VI_Github_Beta_Sandbox import (main) #main_multiday_V2
+from PF_analysis import (main) #main_multiday_V2
                               
 
 #%%
@@ -32,16 +32,16 @@ datatype = ['dec']# 'spikes'
 N_shuffles = 200
 pc_meth = 'SHC' 
 for m in range(len(Mouse)):
-    dirct = r'C:\Users\Vlad\Desktop\BCF\Alis_data\Data\Mouse%d' %Mouse[m]
+    dirct = #r'...\Data\Mouse%d' %Mouse[m]
     Days_dict = {'%d'%(j+1): days[m][j] for j in range(len(days[m]))}
-    path = [r'C:\Users\Vlad\Desktop\BCF\Alis_data\Data\Mouse%d\Day%d' %(Mouse[m], days[m][j]) for j in range(len(days[m]))]
+    path = #[r'...\Data\Mouse%d\Day%d' %(Mouse[m], days[m][j]) for j in range(len(days[m]))]
     trajname = [sorted(glob.glob(os.path.join(path[i], '*.csv'))) for i in range(len(path))]
     print('Digesting data for Mouse %d, %d of %d' %(Mouse[m], m+1, len(Mouse)))
 
     for j in range(len(datatype)):
-        names = [dirct + r'\comparison\Data_Mouse%d_new_multiday.hdf5' %Mouse[m], trajname]
-        savenames = [[dirct + r'\comparison\data_outputs\Mouse%d_day%d_%s_%dshuff_adaptedshuffles_circshuffles_%s.mat' 
-                      %(Mouse[m], days[m][i], datatype[j],N_shuffles, pc_meth) 
+        names = [dirct + r'\Data_Mouse%d_new_multiday.hdf5' %Mouse[m], trajname]
+        savenames = [[dirct + #r'\data_outputs\Mouse%d_day%d_%s_%dshuff_adaptedshuffles_circshuffles_%s.mat' 
+                      %(Mouse[m], days[m][i], datatype[j], N_shuffles, pc_meth) 
                       for i in range(len(days[m]))],\
                      dirct,\
                      [dirct + r'\text_outputs\%s-Mouse%d_day%d_%s_output_%dshuff_adaptedshuffles_circshuffles_%s.txt'
@@ -50,3 +50,4 @@ for m in range(len(Mouse)):
             
         main(names, datatype[j], savenames, Days_dict, N_shuffles=N_shuffles, 
              pc_meth=pc_meth)
+
